c2c08320c5eeefcb4fe093a0899cdcb5f8259bd4
pycharmlover <2783194591@qq.com>
2026-03-14 16:28:05 +0800

    实现搜索功能

--- frontend/src/components/navbar/NavBar.vue
+++ frontend/src/components/navbar/NavBar.vue
@@ -1,14 +1,33 @@
-<script setup>
+<script setup xmlns="">
 
-import MenuIcon from "@/components/navbar/icons/MenuIcon.vue";
-import HomePageIcon from "@/components/navbar/icons/HomePageIcon.vue";
-import FriendIcon from "@/components/navbar/icons/FriendIcon.vue";
-import CreateIcon from "@/components/navbar/icons/CreateIcon.vue";
-import SearchIcon from "@/components/navbar/icons/SearchIcon.vue";
-import {useUserStore} from "@/stores/user.js"
-import UserMenu from "@/components/UserMenu.vue";
+import {useUserStore} from "../../stores/user.js";
+import MenuIcon from "./icons/MenuIcon.vue";
+import SearchIcon from "./icons/SearchIcon.vue";
+import CreateIcon from "./icons/CreateIcon.vue";
+import UserMenu from "../UserMenu.vue";
+import HomepageIcon from "./icons/HomePageIcon.vue";
+import FriendIcon from "./icons/FriendIcon.vue";
+import {ref, watch} from "vue";
+import {useRoute, useRouter} from "vue-router";
+
 
 const user = useUserStore()
+const searchQuery = ref('')
+const router = useRouter()
+const route = useRoute()
+
+watch(() => route.query.q, newQ => {
+  searchQuery.value = newQ || ''
+})
+
+function handleSearch() {
+  router.push({
+    name: 'homepage-index',
+    query: {
+      q: searchQuery.value.trim(),
+    }
+  })
+}
 </script>
 
 <template>
@@ -23,13 +42,13 @@ const user = useUserStore()
           <div class="px-2 font-bold text-xl">AIFriends</div>
         </div>
         <div class="navbar-center w-4/5 max-w-180 flex justify-center">
-          <div class="join w-4/5 flex justify-center">
-            <input class="input join-item rounded-l-full w-4/5" placeholder="搜索你感兴趣的内容" />
+          <form @submit.prevent="handleSearch" class="join w-4/5 flex justify-center">
+            <input v-model="searchQuery" class="input join-item rounded-l-full w-4/5" placeholder="搜索你感兴趣的内容" />
             <button class="btn join-item rounded-r-full gap-0">
               <SearchIcon />
               搜索
             </button>
-          </div>
+          </form>
         </div>
         <div class="navbar-end">
           <RouterLink v-if="user.isLogin()" :to="{name: 'create-index'}" active-class="btn-active" class="btn btn-ghost text-base mr-6">
